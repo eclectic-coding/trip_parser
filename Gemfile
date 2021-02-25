dab97eaa8422ad1341c939170825d16a633908b9
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-02-25 13:20:33 -0500

    Finish total_distance method to calculate distance per driver

--- Gemfile
+++ Gemfile
@@ -10,5 +10,6 @@ gem "rake", "~> 13.0"
 group :development, :test do
   gem "minitest", "~> 5.0"
   gem "minitest-reporters"
+  gem "pry"
   gem "rubocop", "~> 0.80"
 end
--- lib/trip_parser/cli.rb
+++ lib/trip_parser/cli.rb
@@ -1,16 +1,44 @@
 # frozen_string_literal: true
 
+require 'pry'
+
 module TripParser
+  ##
+  # Main CLI interface
+  #
   class CLI
 
     def call
       puts "Hello y'all"
+      # find_drivers
+      find_total_distance("Kumi")
     end
 
-    class TripParser < StandardError
-      def message
-        puts "You messaged up"
+    def find_drivers
+      drivers = []
+
+      File.foreach("input.txt") do |line|
+        drivers << line.partition(" ").last.strip if line.partition(" ").first == "Driver"
       end
+      drivers.uniq
     end
+
+    def find_total_distance(driver)
+      miles = 0.0
+      File.foreach("input.txt") do |trip|
+        miles += trip.split(" ")[4].to_f if trip.scan("Trip #{driver.strip}").first
+      end
+
+      puts miles.positive? ? miles : 0
+    end
+
+    def find_average_trips_speed; end
+
+    def output_report; end
+
+    ##
+    # Main TripParser Error
+    #
+    class TripParser < StandardError; end
   end
 end
